c44290405ad2f74fc12453f78f8e39c6d8dcc707
Umesh Unnikrishnan <umeshvu@outlook.com>
2025-02-25 20:56:40 +0530

    adding listing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,95 @@
-import {useState} from 'react';
+import  {useState} from 'react';
 import './App.css'
-import _ from 'lodash';
 
-function App() {
-    // const [rawData, setRawData] = useState(null);
-    const [data, setData] = useState(null);
+// eslint-disable-next-line react/prop-types
+const DataList = ({data}) => {
+    const [editableData, setEditableData] = useState(data);
+    const [tempData, setTempData] = useState(data);
+    const [isEditing, setIsEditing] = useState(false);
+    const [isListing, setIsListing] = useState(false);
 
-    const fetchData = async () => {
-        try {
-            // Fetch data from an API
-            const response = await fetch('https://api.github.com/users/corollari/repos');
-            const rawDataReceived = await response.json();
-
-            // Set the raw data to state
-            // setRawData(rawDataReceived);
-
-            // Process the data using map
-            let processedData = rawDataReceived.map(item => item.name);
-
-            // let pairs = _.pairs(processedData);
-            // let insertPosition = _.findIndex(pairs, function(pair) {
-            //     return pair[0] === "0";
-            // });
-//
-// // Insert the new key-value pair
-//             let newPair = ["twelve", "12"];
-//             pairs.splice(insertPosition, 0, newPair);
-//
-// // Convert the array back to an object
-//             let newObject = _.object(pairs);
-
-            // processedData = {"132":{id:123,name:"umesh"},...processedData};
-
-            // Set the processed data to state
-            setData(processedData);
-            console.log(processedData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+    const handleEditClick = () => {
+        setIsEditing(true);
     };
 
-    // const fetchData = () => {
-    //   // Fetch data from an API
-    //   fetch('https://api.github.com/users/corollari/repos')
-    //   .then(response => response.json())
-    //   .then(rawDataReceived => {
-    //     setRawData(rawDataReceived);
-    //     // Process the data using map
-    //     const processedData = rawDataReceived.map(item => ({
-    //       id: item.id,
-    //       name: item.name
-    //     }));
-    //     return processedData;
-    //   })
-    //   .then(data => {
-    //     setData(data);
-    //   })
-    //   .catch(error => {
-    //     console.error('Error fetching data:', error);
-    //   });
-    // };
+    const handleSaveClick = () => {
+        setIsEditing(false);
+        setEditableData(tempData);
+        console.log('Saved data:', tempData);
+        // Here you can add logic to save the data to a server
+    };
 
+    const handleStartListingClick = () => {
+        setIsListing(!isListing);
+    };
 
-    function insertKeyInPosition(obj, newKey, newValue, position) {
-        // Create a new object to hold the reordered properties
-        const newObj = {};
-        let index = 0;
-
-        // Iterate over the original object's keys
-        for (const key in obj) {
-            if (index === position) {
-                // Insert the new key-value pair at the specified position
-                newObj[newKey] = newValue;
-            }
-            // Copy the existing key-value pair to the new object
-            newObj[key] = obj[key];
-            index++;
-        }
-
-        // If the position is at the end or beyond the current keys
-        if (index <= position) {
-            newObj[newKey] = newValue;
-        }
-
-        return newObj;
-    }
+    const handleInputChange = (event, section, itemKey) => {
+        const newData = {...tempData};
+        newData[section][itemKey] = event.target.value;
+        setTempData(newData);
+    };
 
     return (
         <div>
-            <button onClick={fetchData}>Fetch Data</button>
-            <p>Processed data</p>
-            {<pre>{JSON.stringify(data, null, '\t')}</pre>}
-            {/*<p>Raw data</p>*/}
-            {/*{<pre>{JSON.stringify(rawData, null, 2)}</pre>}*/}
+            {isListing ? (
+                <div>
+                    {Object.keys(editableData).map((section, index) => (
+                        <div key={index}>
+                            <h2>{section}</h2>
+                            <ul>
+                                {Object.keys(editableData[section]).map((itemKey) => (
+                                    <li key={itemKey}>
+                                        {isEditing ? (
+                                            <input
+                                                type="text"
+                                                value={tempData[section][itemKey]}
+                                                onChange={(event) => handleInputChange(event, section, itemKey)}
+                                            />
+                                        ) : (
+                                            editableData[section][itemKey]
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                    {isEditing ? (
+                        <button onClick={handleSaveClick}>Save</button>
+                    ) : (
+                        <button onClick={handleEditClick}>Edit</button>
+                    )}
+                </div>
+            ) : (<p>...</p>
+            )}
+            <button onClick={handleStartListingClick}>Start Listing</button>
         </div>
     );
-}
+};
 
-export default App
+const App = () => {
+    const data = {
+        one: {
+            "1": "this is one",
+            "2": "this is two",
+            "3": "this is three"
+        },
+        two: {
+            "1": "this is one",
+            "2": "this is two",
+            "3": "this is three"
+        },
+        three: {
+            "1": "this is one",
+            "2": "this is two",
+            "3": "this is three"
+        }
+    };
+
+    return (
+        <div>
+            <DataList data={data}/>
+        </div>
+    );
+};
+
+export default App;
